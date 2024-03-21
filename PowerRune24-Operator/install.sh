47f2a34cb685db7a61bf9140d438e59e6802319a
ChengHao <chengnangong@163.com>
2024-03-21 12:12:05 +0800

    update bat

--- PowerRune24-Operator/install.sh
+++ PowerRune24-Operator/install.sh
@@ -1,15 +1,21 @@
 # python3, textual环境部署
 echo "Enploying PowerRune Operator environment..."
-# 1. 检查python3环境，包括python3和pip3
-version=$(python3 -V 2>&1 | awk '{print $2}')
-version_pip=$(pip3 -V 2>&1 | awk '{print $2}')
+# 1. 检查python3环境，包括python3和pip3，python3版本必须大于3.7
+version=$(python3 -V 2>&1 | grep -Po '(?<=Python )(\d+\.\d+)' || echo "")
+version_pip=$(pip3 -V 2>&1 | grep -Po '(?<=python )(\d+\.\d+)' || echo "")
 
 if [ -z "$version" ]; then
-    echo "Python3 is not installed, please install it first."
+    echo "Python3 is not installed."
     exit 1
 fi
+
+if [ $(echo "$version < 3.7" | bc) -eq 1 ]; then
+    echo "Python3 version must be greater than 3.7."
+    exit 1
+fi
+
 if [ -z "$version_pip" ]; then
-    echo "Pip3 is broken, please fix it first."
+    echo "Pip3 is not installed."
     exit 1
 fi
 
